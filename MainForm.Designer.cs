bec99c7b1d1f57b186acee4e8eb09427524b129d
Felix <xfelixe@gmail.com>
2024-11-17 09:33:57 +0100

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace TicketSystem
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ticketsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new TicketSystem.Database1DataSet();
+            this.ticketsTableAdapter = new TicketSystem.Database1DataSetTableAdapters.TicketsTableAdapter();
+            this.tableAdapterManager = new TicketSystem.Database1DataSetTableAdapters.TableAdapterManager();
+            this.ticketsBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.severityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtTicketID = new System.Windows.Forms.TextBox();
+            this.btnSearchID = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnCreateTicket = new System.Windows.Forms.Button();
+            this.cboxSeverity = new System.Windows.Forms.ComboBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticketsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticketsBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle10.Format = "N0";
+            dataGridViewCellStyle10.NullValue = "1";
+            dataGridViewCellStyle10.SelectionBackColor = System.Drawing.Color.DarkTurquoise;
+            dataGridViewCellStyle10.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.severityDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.status});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView1.DataSource = this.ticketsBindingSource;
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle12.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle12.Padding = new System.Windows.Forms.Padding(3);
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle12;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.Color.Black;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 152);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(916, 340);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ticketsBindingSource
+            // 
+            this.ticketsBindingSource.DataMember = "Tickets";
+            this.ticketsBindingSource.DataSource = this.database1DataSet;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // ticketsTableAdapter
+            // 
+            this.ticketsTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CustomersTableAdapter = null;
+            this.tableAdapterManager.TicketsTableAdapter = this.ticketsTableAdapter;
+            this.tableAdapterManager.UpdateOrder = TicketSystem.Database1DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            this.tableAdapterManager.UsersTableAdapter = null;
+            // 
+            // ticketsBindingSource1
+            // 
+            this.ticketsBindingSource1.DataMember = "Tickets";
+            this.ticketsBindingSource1.DataSource = this.database1DataSet;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle11.Padding = new System.Windows.Forms.Padding(3);
+            this.idDataGridViewTextBoxColumn.DefaultCellStyle = dataGridViewCellStyle11;
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // severityDataGridViewTextBoxColumn
+            // 
+            this.severityDataGridViewTextBoxColumn.DataPropertyName = "severity";
+            this.severityDataGridViewTextBoxColumn.DataSource = this.ticketsBindingSource;
+            this.severityDataGridViewTextBoxColumn.DisplayMember = "severity";
+            this.severityDataGridViewTextBoxColumn.DisplayStyle = System.Windows.Forms.DataGridViewComboBoxDisplayStyle.Nothing;
+            this.severityDataGridViewTextBoxColumn.DropDownWidth = 4;
+            this.severityDataGridViewTextBoxColumn.HeaderText = "severity";
+            this.severityDataGridViewTextBoxColumn.MaxDropDownItems = 4;
+            this.severityDataGridViewTextBoxColumn.MinimumWidth = 100;
+            this.severityDataGridViewTextBoxColumn.Name = "severityDataGridViewTextBoxColumn";
+            this.severityDataGridViewTextBoxColumn.ReadOnly = true;
+            this.severityDataGridViewTextBoxColumn.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.severityDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            this.severityDataGridViewTextBoxColumn.ValueMember = "severity";
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "description";
+            this.descriptionDataGridViewTextBoxColumn.MinimumWidth = 200;
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // status
+            // 
+            this.status.DataPropertyName = "status";
+            this.status.HeaderText = "status";
+            this.status.Name = "status";
+            this.status.ReadOnly = true;
+            // 
+            // txtTicketID
+            // 
+            this.txtTicketID.Location = new System.Drawing.Point(135, 127);
+            this.txtTicketID.Name = "txtTicketID";
+            this.txtTicketID.Size = new System.Drawing.Size(144, 20);
+            this.txtTicketID.TabIndex = 3;
+            this.txtTicketID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnSearchID
+            // 
+            this.btnSearchID.Location = new System.Drawing.Point(12, 124);
+            this.btnSearchID.Name = "btnSearchID";
+            this.btnSearchID.Size = new System.Drawing.Size(117, 23);
+            this.btnSearchID.TabIndex = 7;
+            this.btnSearchID.Text = "Search ID";
+            this.btnSearchID.UseVisualStyleBackColor = true;
+            this.btnSearchID.Click += new System.EventHandler(this.btnSearchID_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 65);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(892, 53);
+            this.btnLoad.TabIndex = 9;
+            this.btnLoad.Text = "Clear / Reset";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnCreateTicket
+            // 
+            this.btnCreateTicket.Location = new System.Drawing.Point(446, 39);
+            this.btnCreateTicket.Name = "btnCreateTicket";
+            this.btnCreateTicket.Size = new System.Drawing.Size(458, 20);
+            this.btnCreateTicket.TabIndex = 10;
+            this.btnCreateTicket.Text = "Create Ticket";
+            this.btnCreateTicket.UseVisualStyleBackColor = true;
+            this.btnCreateTicket.Click += new System.EventHandler(this.btnCreateTicket_Click);
+            // 
+            // cboxSeverity
+            // 
+            this.cboxSeverity.FormattingEnabled = true;
+            this.cboxSeverity.Items.AddRange(new object[] {
+            "0 (low)",
+            "1 (low-medium)",
+            "2 (medium)",
+            "3 (bad)",
+            "4 (critical)",
+            "5 (very critical)"});
+            this.cboxSeverity.Location = new System.Drawing.Point(446, 12);
+            this.cboxSeverity.Name = "cboxSeverity";
+            this.cboxSeverity.Size = new System.Drawing.Size(458, 21);
+            this.cboxSeverity.TabIndex = 11;
+            this.cboxSeverity.Text = "Severity";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(12, 12);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(428, 47);
+            this.txtDescription.TabIndex = 12;
+            this.txtDescription.Text = "Add description";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(916, 492);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.cboxSeverity);
+            this.Controls.Add(this.btnCreateTicket);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSearchID);
+            this.Controls.Add(this.txtTicketID);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "Ticket System";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticketsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticketsBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource ticketsBindingSource;
+        private Database1DataSetTableAdapters.TicketsTableAdapter ticketsTableAdapter;
+        private Database1DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingSource ticketsBindingSource1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn severityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+        private System.Windows.Forms.TextBox txtTicketID;
+        private System.Windows.Forms.Button btnSearchID;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnCreateTicket;
+        private System.Windows.Forms.ComboBox cboxSeverity;
+        private System.Windows.Forms.TextBox txtDescription;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace TicketSystem
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataAdapter adapter;
+        private SqlDataReader reader;
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadTickets()
+        {
+            command = new SqlCommand("SELECT * FROM Tickets", connection);
+            adapter = new SqlDataAdapter(command);
+
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.Tickets' table. You can move, or remove it, as needed.
+            this.ticketsTableAdapter.Fill(this.database1DataSet.Tickets);
+
+            connection =
+                new SqlConnection(
+                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Felix\source\repos\TicketSystem\Database1.mdf;Integrated Security=True");
+
+            LoadTickets();
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveInfo();
+        }
+
+        private void CreateTicket()
+        {
+            //string query = "INSERT into Tickets " + () + "VALUES (@Id, @Severity, @Description)";
+        }
+
+        private void SaveInfo()
+        {
+            //string query = "INSERT into Customers" + 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearchID_Click(object sender, EventArgs e)
+        {
+            var searchID = txtTicketID.Text;
+
+            if (string.IsNullOrEmpty(searchID))
+            {
+                MessageBox.Show("Please enter a valid ID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                connection.Open();
+                command.CommandText = $"SELECT * FROM Tickets WHERE ID = @ID";
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("@ID", searchID);
+
+                reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    var dataTable = new DataTable();
+                    dataTable.Load(reader);
+                    dataGridView1.DataSource = dataTable;
+                }
+                else
+                {
+                    LoadTickets();
+                    MessageBox.Show("No records found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.InnerException}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                reader?.Close();
+                connection.Close();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetUI()
+        {
+            txtTicketID.Text = "";
+            txtDescription.Text = "";
+            cboxSeverity.Text = "";
+            dataGridView1.ClearSelection();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadTickets();
+            ResetUI();
+        }
+
+        private void btnCreateTicket_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CreateTicket();
+                MessageBox.Show("Ticket created successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadTickets();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while creating the ticket {ex.InnerException}", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+    }
+}
